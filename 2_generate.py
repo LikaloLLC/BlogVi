6d199a353a0ee9312f4cf651fef632e70e2bbec2
zarif99 <9685440@gmail.com>
2021-04-05 19:17:13 +0500

    Generate landing page   * Generating each markdown file to HTML   * Parsing data csv file

--- 2_generate.py
+++ 2_generate.py
@@ -1,46 +1,115 @@
-#TaSaB - Tailwindcss Static Blog (with Search)
-import markdown
-from jinja2 import Template
-from utils import ImgExtExtension, H1H2Extension
-import pandas as pd
-import requests
-#https://docs.google.com/spreadsheets/d/1deKANndKOOmOdQUQWDK6-zC7P6J25SzBrUx2RX9lvkY/gviz/tq?tqx=out:csv&sheet=Form%20Responses%201
-from io import StringIO
+import glob
+from datetime import datetime
 
+import markdown
+from jinja2 import FileSystemLoader, Environment
+
+from utils import get_data, ImgExtExtension, H1H2Extension
+
+
+# https://docs.google.com/spreadsheets/d/1deKANndKOOmOdQUQWDK6-zC7P6J25SzBrUx2RX9lvkY/gviz/tq?tqx=out:csv&sheet=Form%20Responses%201
 
 
 class Blog():
-    def generate_categories(self):
-        pass
+
+  def __init__(self, timestamp=None, header_image=None, author_name=None, author_image=None, author_email=None,
+               summary=None, categories=None, markdown=None, detail_url=None,
+               author_info=None, author_social=None, status=None):
+    self.author_name = author_name
+    self.markdown = markdown
+    self.header_image = header_image
+    self.author_image = author_image
+    self.author_email = author_email
+    self.summary = summary
+    self.timestamp = timestamp
+    self.categories = categories
+    self.author_info = author_info
+    self.author_social = author_social
+    self.status = status
+    self.detail_url = detail_url
+
+  def generate_categories(self):
+    pass
+
+  def generate_landing_page(self):
+    md = markdown.Markdown(extensions=[ImgExtExtension(), H1H2Extension()])
+    directory_loader = FileSystemLoader('templates')
+
+    env = Environment(loader=directory_loader)
+    tm = env.get_template('base_index.html')
+
+    for html_file in glob.glob('articles/*.html'):
+      name = html_file
+      name = name.replace("articles/", "")
+      with open(html_file, 'r') as f:
+        html_file = f.read()
+
+      ms = tm.render(html_content=html_file)
+      self.detail_url = f'templates/blogs/{name}'
+      with open(f'templates/blogs/{name}', 'w') as f:
+        f.write(ms)
+
+    # header_image = file.images[0]
+    # name_image = md.h1s
+
+    directory_loader = FileSystemLoader('templates')
+    env = Environment(loader=directory_loader)
+    tm = env.get_template('base_landing.html')
+
+    templates = []
+
+    for file in glob.glob("templates/tailwind_g/includes/*.html"):
+      file = file.replace("templates/", "")
+      templates.append(file)
+
+    ms = tm.render(templates=templates, blog=self)
+
+    with open('templates/landing.html', 'w') as f:
+      f.write(ms)
+
+  def generate_articles(self):
+    for md_file in glob.glob('articles/*.md'):
+      md_file.replace("articles/", "")
+      markdown.markdownFromFile(input=md_file, output=f'{md_file[:len(md_file) - 3]}.html')
+
+  def generate_rss(self):
+    pass
+
+  def create_search_index(self):
+    pass
+
+  def create(self):
+    directory_loader = FileSystemLoader('templates')
+    env = Environment(loader=directory_loader)
+
+    tm = env.get_template('blog_schema.html')
+    name_file = (str(datetime.now()) + "|test").replace('.', '')
+    ms = tm.render(blog=self, url=f"tailwind_g/includes/{name_file}.html")
+
+    with open(f'templates/tailwind_g/includes/{name_file}.html', mode="w") as f:
+      f.write(ms)
 
 
-    def generate_landing_page(self):
+def main():
+  url = "https://docs.google.com/spreadsheets/d/1deKANndKOOmOdQUQWDK6-zC7P6J25SzBrUx2RX9lvkY/gviz/tq?tqx=out:csv&sheet=Form%20Responses%201"
+  datas = get_data(url)
+  blog = Blog()
+  blog.generate_articles()
+  for data in datas:
+    # if data['Status']:
+    blog.author_name = data['Author Name']
+    blog.markdown = data['Markdown']
+    blog.author_image = data['Author Avatar Image URL']
+    blog.categories = data['Categories ']
+    blog.summary = data['Excerpt/Short Summary']
+    blog.header_image = data['Header Image (will be used in RSS feed)']
+    blog.author_info = data['About the Author']
+    blog.timestamp = data['Timestamp']
+    blog.detail_url = None
+    blog.create()
 
-        md = markdown.Markdown(extensions=[ImgExtExtension(), H1H2Extension()])
-
-        file = md.convertFile('articles/ciaran_1.md')
-
-        header_image = file.images[0]
-        name_image = md.h2s
-
-
-
-    def generate_articles(self):
-        pass
-
-
-    def generate_rss(self):
-        pass
-
-
-    def create_search_index(self):
-        pass
-
-
-
-
+  blog.generate_landing_page()
 
 
 if __name__ == '__main__':
-    pass
-
+  main()
--- utils.py
+++ utils.py
@@ -1,8 +1,12 @@
-import markdown
+import csv
+
+import requests
+from markdown import Extension
 from markdown.treeprocessors import Treeprocessor
-from markdown.extensions import Extension
+
 
 # First create the treeprocessor
+
 
 class ImgExtractor(Treeprocessor):
     def run(self, doc):
@@ -11,13 +15,13 @@ class ImgExtractor(Treeprocessor):
         for image in doc.findall('.//img'):
             self.markdown.images.append(image.get('src'))
 
+
 # Then tell markdown about it
 
 class ImgExtExtension(Extension):
     def extendMarkdown(self, md, md_globals):
         img_ext = ImgExtractor(md)
         md.treeprocessors.add('imgext', img_ext, '>inline')
-
 
 
 class NameDescriptionExtractor(Treeprocessor):
@@ -35,3 +39,28 @@ class H1H2Extension(Extension):
     def extendMarkdown(self, md, md_globals):
         h1h2_ext = NameDescriptionExtractor(md)
         md.treeprocessors.add('h1h2ext', h1h2_ext, '>inline')
+
+
+def make_json(file):
+    data = []
+    # read records from data.csv file and convert it list
+    with open('data.csv', encoding='utf-8') as f:
+        csvReader = csv.DictReader(f)
+        for rows in csvReader:
+            data.append(rows)
+    # print(data)
+    return data
+
+
+def get_data(url: str):
+    response = requests.get(url=url)
+    # write records to data.csv file
+    with open('data.csv', 'wb') as f:
+        f.write(response.content)
+
+    # with open('data.csv') as f:
+    data = make_json('data.csv')
+    return data
+
+# url = "https://docs.google.com/spreadsheets/d/1deKANndKOOmOdQUQWDK6-zC7P6J25SzBrUx2RX9lvkY/gviz/tq?tqx=out:csv&sheet=Form%20Responses%201"
+# get_data(url)
