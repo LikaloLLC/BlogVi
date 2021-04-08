49d5c2d8bc554219a7f7bd52a7c91de888ef2244
zarif99 <9685440@gmail.com>
2021-04-08 23:59:35 +0500

    Please run the 2_generate.py and open landing.html file in browser, and you can see blog cards

--- 2_generate.py
+++ 2_generate.py
@@ -1,20 +1,26 @@
 import glob
-from datetime import datetime
+import os
+import random
 
 import markdown
 from jinja2 import FileSystemLoader, Environment
 
-from utils import get_data, ImgExtExtension, H1H2Extension
+from utils import get_data, get_md_file, ImgExtExtension, H1H2Extension
 
-
+# Philippe`s google Sheet
 # https://docs.google.com/spreadsheets/d/1deKANndKOOmOdQUQWDK6-zC7P6J25SzBrUx2RX9lvkY/gviz/tq?tqx=out:csv&sheet=Form%20Responses%201
+
+# Zarif`s google sheet
+# "https://docs.google.com/spreadsheets/d/e/2PACX-1vQNYmtcxqYplBu7SlY6grJRb3Y0vrmOBkE8j2CyeQlQHq4ElynBfi0Tkd4h2u2tPj4EmeGFBxyy8g73/pub?output=csv"
+
+blogs_list = []
 
 
 class Blog():
 
   def __init__(self, timestamp=None, header_image=None, author_name=None, author_image=None, author_email=None,
                summary=None, categories=None, markdown=None, detail_url=None,
-               author_info=None, author_social=None, status=None):
+               author_info=None, author_social=None, status=None) -> None:
     self.author_name = author_name
     self.markdown = markdown
     self.header_image = header_image
@@ -28,49 +34,59 @@ class Blog():
     self.status = status
     self.detail_url = detail_url
 
+  def __str__(self) -> str:
+    return f"Blog is about {self.author_name}"
+
+  def all(self) -> dict:
+    dct = {
+      'author_name': self.author_name,
+      'author_email': self.author_email,
+      'author_info': self.author_info,
+      'author_image': self.author_image,
+      'author_social': self.author_social,
+      'markdown': self.markdown,
+      'header_image': self.header_image,
+      'summary': self.summary,
+      'timestamp': self.timestamp,
+      'categories': self.categories,
+      'status': self.status,
+      'detail_url': self.detail_url
+    }
+    return dct
+
   def generate_categories(self):
     pass
 
-  def generate_landing_page(self):
+  def generate_landing_page(self) -> None:
     md = markdown.Markdown(extensions=[ImgExtExtension(), H1H2Extension()])
-    directory_loader = FileSystemLoader('templates')
+    try:
 
-    env = Environment(loader=directory_loader)
-    tm = env.get_template('base_index.html')
+      directory_loader = FileSystemLoader('templates')
+      env = Environment(loader=directory_loader)
+      tm = env.get_template('base_landing.html')
 
-    for html_file in glob.glob('articles/*.html'):
-      name = html_file
-      name = name.replace("articles/", "")
-      with open(html_file, 'r') as f:
-        html_file = f.read()
+      n = random.randint(0, len(blogs_list))
+      ms = tm.render(blogs=blogs_list, head_blog=blogs_list[n])
+      # print(blogs_list)
 
-      ms = tm.render(html_content=html_file)
-      self.detail_url = f'templates/blogs/{name}'
-      with open(f'templates/blogs/{name}', 'w') as f:
+      with open('templates/landing.html', 'w') as f:
         f.write(ms)
+        print("Succes")
+    except:
+      raise Exception("Something went wrong")
 
-    # header_image = file.images[0]
-    # name_image = md.h1s
+  def generate_articles(self) -> None:
+    file_name = self.author_name + self.timestamp
+    md_file = get_md_file(self.markdown, file_name)
+    file_name = file_name.replace("templates/articles/", "")
+    self.detail_url = f'{file_name}.html'
+    # print(self.detail_url)
 
-    directory_loader = FileSystemLoader('templates')
-    env = Environment(loader=directory_loader)
-    tm = env.get_template('base_landing.html')
+    blogs_list.append(self.all())
 
-    templates = []
-
-    for file in glob.glob("templates/tailwind_g/includes/*.html"):
-      file = file.replace("templates/", "")
-      templates.append(file)
-
-    ms = tm.render(templates=templates, blog=self)
-
-    with open('templates/landing.html', 'w') as f:
-      f.write(ms)
-
-  def generate_articles(self):
-    for md_file in glob.glob('articles/*.md'):
-      md_file.replace("articles/", "")
-      markdown.markdownFromFile(input=md_file, output=f'{md_file[:len(md_file) - 3]}.html')
+    markdown.markdownFromFile(input=md_file, output=f'{md_file[:len(md_file) - 3]}.html')
+    for file in glob.glob('templates/articles/*.md'):
+      os.remove(file)
 
   def generate_rss(self):
     pass
@@ -78,38 +94,43 @@ class Blog():
   def create_search_index(self):
     pass
 
-  def create(self):
+  def create_blog(self) -> None:
     directory_loader = FileSystemLoader('templates')
     env = Environment(loader=directory_loader)
 
-    tm = env.get_template('blog_schema.html')
-    name_file = (str(datetime.now()) + "|test").replace('.', '')
-    ms = tm.render(blog=self, url=f"tailwind_g/includes/{name_file}.html")
+    tm = env.get_template('base_index.html')
 
-    with open(f'templates/tailwind_g/includes/{name_file}.html', mode="w") as f:
+    ms = tm.render(blog=f'articles/{self.detail_url}')
+
+    with open(f'templates/blogs/{self.detail_url}', mode="w") as f:
       f.write(ms)
 
+    self.detail_url = f'templates/blogs/{self.detail_url}'
+    # print(self.detail_url)
 
-def main():
-  url = "https://docs.google.com/spreadsheets/d/1deKANndKOOmOdQUQWDK6-zC7P6J25SzBrUx2RX9lvkY/gviz/tq?tqx=out:csv&sheet=Form%20Responses%201"
+
+def main() -> None:
+  # url = "https://docs.google.com/spreadsheets/d/1deKANndKOOmOdQUQWDK6-zC7P6J25SzBrUx2RX9lvkY/gviz/tq?tqx=out:csv&sheet=Form%20Responses%201"
+  url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQNYmtcxqYplBu7SlY6grJRb3Y0vrmOBkE8j2CyeQlQHq4ElynBfi0Tkd4h2u2tPj4EmeGFBxyy8g73/pub?output=csv"
   datas = get_data(url)
   blog = Blog()
-  blog.generate_articles()
   for data in datas:
-    # if data['Status']:
-    blog.author_name = data['Author Name']
-    blog.markdown = data['Markdown']
-    blog.author_image = data['Author Avatar Image URL']
-    blog.categories = data['Categories ']
-    blog.summary = data['Excerpt/Short Summary']
-    blog.header_image = data['Header Image (will be used in RSS feed)']
-    blog.author_info = data['About the Author']
-    blog.timestamp = data['Timestamp']
-    blog.detail_url = None
-    blog.create()
-
+    if data['Status']:
+      blog.author_name = data['Author Name']
+      blog.author_email = data['Author email']
+      blog.author_info = data['About the Author']
+      blog.author_image = data['Author Avatar Image URL']
+      blog.author_social = data['linked.in github urls']
+      blog.summary = data['Excerpt/Short Summary']
+      blog.categories = data['Categories '].split(" ")
+      blog.timestamp = data['Отметка времени']
+      blog.markdown = data['Markdown']
+      blog.generate_articles()
+      blog.create_blog()
+      print(blog.categories)
   blog.generate_landing_page()
 
 
 if __name__ == '__main__':
+  print("Running")
   main()
--- utils.py
+++ utils.py
@@ -9,58 +9,67 @@ from markdown.treeprocessors import Treeprocessor
 
 
 class ImgExtractor(Treeprocessor):
-    def run(self, doc):
-        "Find all images and append to markdown.images. "
-        self.markdown.images = []
-        for image in doc.findall('.//img'):
-            self.markdown.images.append(image.get('src'))
+  def run(self, doc):
+    "Find all images and append to markdown.images. "
+    self.markdown.images = []
+    for image in doc.findall('.//img'):
+      self.markdown.images.append(image.get('src'))
 
 
 # Then tell markdown about it
 
 class ImgExtExtension(Extension):
-    def extendMarkdown(self, md, md_globals):
-        img_ext = ImgExtractor(md)
-        md.treeprocessors.add('imgext', img_ext, '>inline')
+  def extendMarkdown(self, md, md_globals):
+    img_ext = ImgExtractor(md)
+    md.treeprocessors.add('imgext', img_ext, '>inline')
 
 
 class NameDescriptionExtractor(Treeprocessor):
-    def run(self, doc):
-        "Find all images and append to markdown.images. "
-        self.markdown.h1s = []
-        for h1 in doc.findall('.//h1'):
-            self.markdown.h1s.append(h1.text)
-        self.markdown.h2s = []
-        for h2 in doc.findall('.//h2'):
-            self.markdown.h2s.append(h2.text)
+  def run(self, doc):
+    "Find all images and append to markdown.images. "
+    self.markdown.h1s = []
+    for h1 in doc.findall('.//h1'):
+      self.markdown.h1s.append(h1.text)
+    self.markdown.h2s = []
+    for h2 in doc.findall('.//h2'):
+      self.markdown.h2s.append(h2.text)
 
 
 class H1H2Extension(Extension):
-    def extendMarkdown(self, md, md_globals):
-        h1h2_ext = NameDescriptionExtractor(md)
-        md.treeprocessors.add('h1h2ext', h1h2_ext, '>inline')
+  def extendMarkdown(self, md, md_globals):
+    h1h2_ext = NameDescriptionExtractor(md)
+    md.treeprocessors.add('h1h2ext', h1h2_ext, '>inline')
 
 
 def make_json(file):
-    data = []
-    # read records from data.csv file and convert it list
-    with open('data.csv', encoding='utf-8') as f:
-        csvReader = csv.DictReader(f)
-        for rows in csvReader:
-            data.append(rows)
-    # print(data)
-    return data
+  data = []
+  # read records from data.csv file and convert it list
+  with open('data.csv', encoding='utf-8') as f:
+    csvReader = csv.DictReader(f)
+    for rows in csvReader:
+      data.append(rows)
+  # print(data)
+  return data
 
 
 def get_data(url: str):
-    response = requests.get(url=url)
-    # write records to data.csv file
-    with open('data.csv', 'wb') as f:
-        f.write(response.content)
+  response = requests.get(url=url)
+  # write records to data.csv file
+  with open('data.csv', 'wb') as f:
+    f.write(response.content)
 
-    # with open('data.csv') as f:
-    data = make_json('data.csv')
-    return data
+  # with open('data.csv') as f:
+  data = make_json('data.csv')
+  return data
+
+
+def get_md_file(markdown_url: str, file_name: str) -> None:
+  response = requests.get(markdown_url)
+
+  with open(f"templates/articles/{file_name}.md", "wb") as f:
+    f.write(response.content)
+
+  return f"templates/articles/{file_name}.md"
 
 # url = "https://docs.google.com/spreadsheets/d/1deKANndKOOmOdQUQWDK6-zC7P6J25SzBrUx2RX9lvkY/gviz/tq?tqx=out:csv&sheet=Form%20Responses%201"
 # get_data(url)
