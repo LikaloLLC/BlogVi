9244e1815e34472017203afeaaecb30fa5981d43
1mace1 <belyaev.nikit@gmail.com>
2021-10-13 21:55:58 +0300

    Fixed generating links that depends on `blog_root_path` setting

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="blog-vi",
-    version="0.2.1",
+    version="0.2.2",
     description="Simple blog with search and comments",
     long_description=long_description,
     long_description_content_type="text/markdown",
