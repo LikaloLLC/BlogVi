a60e0dd969d824f5fd5e19404e607ae3745e5b62
zarif99 <zariff@docsie.io>
2023-09-05 09:44:15 +0500

    Upgrade version for deploying project to PyPi

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="blog-vi",
-    version="0.2.5",
+    version="1.0.0",
     description="Simple blog with search and comments",
     long_description=long_description,
     long_description_content_type="text/markdown",
